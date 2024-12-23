e1bd45faeaf1bf703c93eee8d6b3a8843af2bb9f
Dannyz <nguyenhuudang0206@gmail.com>
2024-12-23 11:09:05 +0700

    [UI] qb-loading - update

--- [qb]/qb-loading/html/app.js
+++ [qb]/qb-loading/html/app.js
@@ -5,40 +5,40 @@ const { ref } = Vue
 const load = Vue.createApp({
   setup () {
     return {
-      CarouselText1: 'You can add/remove items, vehicles, jobs & gangs through the shared folder.',
-      CarouselSubText1: 'Photo captured by: Markyoo#8068',
-      CarouselText2: 'Adding additional player data can be achieved by modifying the qb-core player.lua file.',
-      CarouselSubText2: 'Photo captured by: ihyajb#9723',
-      CarouselText3: 'All server-specific adjustments can be made in the config.lua files throughout the build.',
-      CarouselSubText3: 'Photo captured by: FLAPZ[INACTIV]#9925',
-      CarouselText4: 'For additional support please join our community at discord.gg/qbcore',
-      CarouselSubText4: 'Photo captured by: Robinerino#1312',
-
-      DownloadTitle: 'Downloading QBCore Server',
-      DownloadDesc: "Hold tight while we begin downloading all the resources/assets required to play on QBCore Server. \n\nAfter download has been finished successfully, you'll be placed into the server and this screen will disappear. Please don't leave or turn off your PC. ",
-
-      SettingsTitle: 'Settings',
-      AudioTrackDesc1: 'When disabled the current audio-track playing will be stopped.',
-      AutoPlayDesc2: 'When disabled carousel images will stop cycling and remain on the last shown.',
-      PlayVideoDesc3: 'When disabled video will stop playing and remain paused.',
-
-      KeybindTitle: 'Default Keybinds',
-      Keybind1: 'Open Inventory',
-      Keybind2: 'Cycle Proximity',
-      Keybind3: 'Open Phone',
-      Keybind4: 'Toggle Seat Belt',
-      Keybind5: 'Open Target Menu',
-      Keybind6: 'Radial Menu',
-      Keybind7: 'Open Hud Menu',
-      Keybind8: 'Talk Over Radio',
-      Keybind9: 'Open Scoreboard',
-      Keybind10: 'Vehicle Locks',
-      Keybind11: 'Toggle Engine',
-      Keybind12: 'Pointer Emote',
-      Keybind13: 'Keybind Slots',
-      Keybind14: 'Hands Up Emote',
-      Keybind15: 'Use Item Slots',
-      Keybind16: 'Cruise Control',
+      // CarouselText1: 'You can add/remove items, vehicles, jobs & gangs through the shared folder.',
+      // CarouselSubText1: 'Photo captured by: Markyoo#8068',
+      // CarouselText2: 'Adding additional player data can be achieved by modifying the qb-core player.lua file.',
+      // CarouselSubText2: 'Photo captured by: ihyajb#9723',
+      // CarouselText3: 'All server-specific adjustments can be made in the config.lua files throughout the build.',
+      // CarouselSubText3: 'Photo captured by: FLAPZ[INACTIV]#9925',
+      // CarouselText4: 'For additional support please join our community at discord.gg/qbcore',
+      // CarouselSubText4: 'Photo captured by: Robinerino#1312',
+      //
+      // DownloadTitle: 'Downloading QBCore Server',
+      // DownloadDesc: "Hold tight while we begin downloading all the resources/assets required to play on QBCore Server. \n\nAfter download has been finished successfully, you'll be placed into the server and this screen will disappear. Please don't leave or turn off your PC. ",
+      //
+      // SettingsTitle: 'Settings',
+      // AudioTrackDesc1: 'When disabled the current audio-track playing will be stopped.',
+      // AutoPlayDesc2: 'When disabled carousel images will stop cycling and remain on the last shown.',
+      // PlayVideoDesc3: 'When disabled video will stop playing and remain paused.',
+      //
+      // KeybindTitle: 'Tổ hợp phím tắt',
+      // Keybind1: 'Mở khoảng không quảng cáo',
+      // Keybind2: 'Chu kỳ gần',
+      // Keybind3: 'Mở điện thoại',
+      // Keybind4: 'Thắt an toàn',
+      // Keybind5: 'Mở menu mục tiêu',
+      // Keybind6: 'Menu xuyên tâm',
+      // Keybind7: 'Mở menu Hud',
+      // Keybind8: 'Talk Over Radio',
+      // Keybind9: 'Open Scoreboard',
+      // Keybind10: 'Vehicle Locks',
+      // Keybind11: 'Toggle Engine',
+      // Keybind12: 'Pointer Emote',
+      // Keybind13: 'Keybind Slots',
+      // Keybind14: 'Hands Up Emote',
+      // Keybind15: 'Use Item Slots',
+      // Keybind16: 'Cruise Control',
 
       firstap: ref(true),
       secondap: ref(true),
@@ -58,7 +58,7 @@ load.use(Quasar, { config: {} })
 load.mount('#loading-main')
 
 var audio = document.getElementById("audio");
-audio.volume = 0.05;
+audio.volume = 1.0;
 
 function audiotoggle() {
     var audio = document.getElementById("audio");
